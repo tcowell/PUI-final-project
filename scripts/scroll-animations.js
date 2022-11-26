264d3d5d8313a5984e1a7a065d701595c80d0438
tcowell <84730021+tcowell@users.noreply.github.com>
2022-11-26 15:02:44 -0500

    reformatted capstone, added scroll animations

---
+++ scripts/scroll-animations.js
@@ -0,0 +1,22 @@
+function scrollTrigger(selector, options = {}) {
+    let els = document.querySelectorAll(selector);
+    els = Array.from(els);
+    els.forEach(el => {
+        addObserver(el, options);
+    })
+}
+
+function addObserver(el) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('active');
+                observer.unobserve(entry.target);
+            }
+        })
+    })
+
+    observer.observe(el);
+}
+
+scrollTrigger('.image-container', {rootMargin: '-200px'})
