bc5f96ebbe28fe9616282f9a4d9776847b73e0f7
tcowell <84730021+tcowell@users.noreply.github.com>
2022-12-03 13:40:10 -0500

    added comments

--- scripts/capstone-charts.js
+++ scripts/capstone-charts.js
@@ -1,92 +1,98 @@
+
+// First doughnut chart
 const ctx = document.getElementById('doughnut-1');
   
-    var chart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['Amount of Feedback'],
-            datasets: [{
-            data: [300],
-            borderWidth: 1,
-            backgroundColor: '#6AAA78'
-            }]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false
-                },
-            },
-            maintainAspectRatio: false,
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Amount of Feedback',
-                    position: 'top',
-                    align: 'center'
+var chart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+        data: [300],
+        borderWidth: 1,
+        backgroundColor: '#6AAA78'
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: '3x increase in Amount of Feedback',
+                position: 'top',
+                align: 'center',
+                font: {
+                    size: 20
                 }
-            }
+            },
+            legend: {
+                display: false
+            },
         }
-    });
+    },
+});
 
-    const ctx2 = document.getElementById('doughnut-2');
+
+// Second doughnut chart
+const ctx2 = document.getElementById('doughnut-2');
   
-    var chart2 = new Chart(ctx2, {
-        type: 'doughnut',
-        data: {
-            labels: ['Usability Increase'],
-            datasets: [{
-            data: [300],
-            borderWidth: 1,
-            backgroundColor: '#6AAA78'
-            }]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false
-                },
-            },
-            maintainAspectRatio: false,
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Usability Increase',
-                    position: 'top',
-                    align: 'center'
+var chart2 = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+        data: [300],
+        borderWidth: 1,
+        backgroundColor: '#6AAA78'
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: '20% Usability Increase',
+                position: 'top',
+                align: 'center',
+                font: {
+                    size: 20
                 }
-            }
-        }
-    });
-
-    const ctx3 = document.getElementById('doughnut-3');
-
-    var chart3 = new Chart(ctx3, {
-        type: 'doughnut',
-        data: {
-            labels: ['Types of Learning Data'],
-            datasets: [{
-            data: [300],
-            borderWidth: 1,
-            backgroundColor: '#6AAA78'
-            }]
-        },
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    display: false
-                },
             },
-            maintainAspectRatio: false,
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Types of Learning Data',
-                    position: 'top',
-                    align: 'center'
-                }
-            }
+            legend: {
+                display: false
+            },
         }
-    });
+    }
+});
+
+
+// Third doughnut chart
+const ctx3 = document.getElementById('doughnut-3');
+
+var chart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+        data: [300],
+        borderWidth: 1,
+        backgroundColor: '#6AAA78'
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            title: {
+                display: true,
+                text: '2x increase in Types of Learning Data',
+                position: 'top',
+                align: 'center',
+                font: {
+                    size: 20
+                }
+            },
+            legend: {
+                display: false
+            },
+        }
+    }
+});
--- scripts/scroll-animations.js
+++ scripts/scroll-animations.js
@@ -1,3 +1,7 @@
+// SCROLL ANIMATIONS FOR INDIVIDUAL ELEMENTS
+// code based on: https://css-tricks.com/scroll-triggered-animation-vanilla-javascript/
+
+// function to gather specific element from the DOM and call addObserver()
 function scrollTrigger(selector, options = {}) {
     let els = document.querySelectorAll(selector);
     els = Array.from(els);
@@ -6,6 +10,8 @@ function scrollTrigger(selector, options = {}) {
     })
 }
 
+// function adds 'active' class to selected element
+// 'active' class is given animation styles in css files
 function addObserver(el) {
     let observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -19,7 +25,33 @@ function addObserver(el) {
     observer.observe(el);
 }
 
+// call scroll trigger for individual elements, each has their own animation style from css files
 scrollTrigger('.image-container', {rootMargin: '-200px'});
 scrollTrigger('.homepage-banner-text', {rootMargin: '-200px'});
 scrollTrigger('.image-row', {rootMargin: '-200px'});
 scrollTrigger('.course-details-section', {rootMargin: '-200px'});
+
+
+
+
+
+
+// CODE FOR FLOATING MENU BANNER
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementByClassName("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
